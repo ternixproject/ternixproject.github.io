8b09f68249a4f5921e8016ad7c6114f00215e5bd
ternixproject <ternix.project@gmail.com>
2024-11-15 14:23:01 +0000

    Update main.js

--- index/main.js
+++ index/main.js
@@ -86,7 +86,7 @@ function updateCountdown() {
     // Calculate hours, minutes, and seconds
     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) //Determine the appropriate labels for hours and minutes
+    const seconds = Math.floor((timeRemaining % (1000 * 60)); //Determine the appropriate labels for hours and minutes
     let hourLabel = (hours === 1) ? 'hr' : 'hrs';
     let minuteLabel = (minutes === 1) ? 'm' : 'm';
 
